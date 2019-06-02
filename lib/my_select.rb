19b825fc177f8dbf719486c8ed6bad135b338de3
Anthony <Anthonywan00@gmail.com>
2019-06-02 13:42:00 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+ # your code here
+        i = 0
+        x = []
+        while i < collection.length
+        yield collection[i]
+            if collection[i].even? == true
+                x.push(collection[i])
+            end
+                i += 1
+        end
+        x
 end
