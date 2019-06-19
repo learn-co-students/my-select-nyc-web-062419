512b013a1557978bbef11b49dcf8603590325c36
adriennem76 <adrienne.c.miranda@gmail.com>
2019-06-19 14:39:30 -0400

    my select

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
+    selected_values = []
+    i = 0
+
+    while i < collection.length
+        if yield(collection[i])
+            selected_values << collection[i]
+        end
+        i += 1
+    end
+    selected_values
  # your code here!
 end
